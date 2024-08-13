3c489d71937c4fbd42dee1f51f06d38e046f9bc5
Nikhil Shah <nikhilshah.net@hotmail.com>
2024-08-13 17:16:25 +0530

    Day 2 - EF core, DI, Fetch API, SOLID

---
+++ Day 2/consume_WebAPI_Fetch/consume_WebAPI_Fetch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace consume_WebAPI_Fetch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
