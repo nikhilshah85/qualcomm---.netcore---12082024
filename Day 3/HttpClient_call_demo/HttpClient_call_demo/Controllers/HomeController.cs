7a119a67a4ffb84261da901e108d69a621931ea9
Nikhil Shah <nikhilshah.net@hotmail.com>
2024-08-14 11:34:48 +0530

    Http Client call

---
+++ Day 3/HttpClient_call_demo/HttpClient_call_demo/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using HttpClient_call_demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HttpClient_call_demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Comments()
+        {
+            CommentsData data = new CommentsData();
+            ViewBag.Comments = data.GetComments();
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Day 3/HttpClient_call_demo/HttpClient_call_demo/Models/CommentsData.cs
@@ -0,0 +1,43 @@
+﻿namespace HttpClient_call_demo.Models
+{
+    public class CommentsData
+    {
+        //declare the properties as per incoming data
+        public int postId { get; set; }
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string    email { get; set; }
+        public string body { get; set; }
+
+        //decalre a variable to catch the data
+
+        List<CommentsData> data = new List<CommentsData>();
+
+
+        //lets make a call
+
+        public List<CommentsData> GetComments()
+        {
+            string url = "https://jsonplaceholder.typicode.com/comments";
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear(); //different calling env will have default data format
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var call = client.GetAsync(url).Result; //this will return HttpStatusCode and referece to stream of incoming data
+                                                    //data will come in chunk / small packets
+
+            if(call.IsSuccessStatusCode) 
+            {
+                    //lets catch the data
+                    var calldata = call.Content.ReadAsAsync<List<CommentsData>>();
+                calldata.Wait();
+                data = calldata.Result; //to our variable                   
+             }
+            else
+            {
+                throw new Exception("Cannot get the data from server, please contact admin");
+            }         
+            return data;
+        }
+    }
+}
